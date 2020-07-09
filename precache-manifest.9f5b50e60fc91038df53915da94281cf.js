0040581dfb9c0968ed6d7c72b784bed8f6883727
Jérémy Simonklein Angély <jeremy.simonklein@gmail.com>
2020-07-09 18:39:19 +0200

    Updates

---
+++ precache-manifest.9f5b50e60fc91038df53915da94281cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553a8b8a64551dd3294398a6674275e4",
+    "url": "/street_wars_landing/public/index.html/index.html"
+  },
+  {
+    "revision": "d99b64e32ecd144aaebb",
+    "url": "/street_wars_landing/public/index.html/static/css/main.9d863687.chunk.css"
+  },
+  {
+    "revision": "32a379c620e063dae196",
+    "url": "/street_wars_landing/public/index.html/static/js/2.48c1018a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/street_wars_landing/public/index.html/static/js/2.48c1018a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d99b64e32ecd144aaebb",
+    "url": "/street_wars_landing/public/index.html/static/js/main.85e9571c.chunk.js"
+  },
+  {
+    "revision": "90353a5ad91d80d1fca8",
+    "url": "/street_wars_landing/public/index.html/static/js/runtime-main.6842248f.js"
+  }
+]);
